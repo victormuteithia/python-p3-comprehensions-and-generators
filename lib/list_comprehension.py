5f550146d29ff7f69b6b4653b0701907edcfa0ea
victormuteithia <muteithiavictor@gmail.com>
2024-03-09 02:31:20 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens_list = [num for num in num_list if num % 2 == 0]
+
+    return evens_list
 
 def make_exclamation(sentence_list):
-    pass
+    added_exclamations = [s + "!" for s in sentence_list]
+
+    return added_exclamations
